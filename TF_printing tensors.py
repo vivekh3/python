cb17082d90818bb4b672bb13112c253c7779d015
vivekh3 <vivek.hari79@gmail.com>
2017-07-27 10:05:13 -0700

    Test for sess.run tensorflow

---
+++ TF_printing tensors.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+sess=tf.InteractiveSession()
+t = tf.constant(42.0)
+u = tf.constant(37.0)
+tu = tf.multiply(t, u)
+ut = tf.multiply(u, t)
+with sess.as_default():
+   tu.eval()  # runs one step
+   ut.eval()  # runs one step
+   sess.run([tu, ut])  # evaluates both tensors in a single step
+
+   
