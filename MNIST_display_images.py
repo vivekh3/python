29de649fec5d84236adfbb05aec4e77a5b49cbdd
vivekh3 <vivek.hari79@gmail.com>
2017-07-28 15:15:28 -0700

    check 12th image

--- MNIST_display_images.py
+++ MNIST_display_images.py
@@ -3,8 +3,8 @@ import cv2
 import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-image_check = mnist.test.images[100]
-label_check = mnist.test.labels[100]
+image_check = mnist.test.images[12]
+label_check = mnist.test.labels[12]
 print (label_check)
 image_check=np.array(image_check,dtype='float32')
 image_check=image_check.reshape((28,28))
