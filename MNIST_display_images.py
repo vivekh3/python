d6774b7a70d0f99ffb64129c47744e17c9eabd06
vivekh3 <vivek.hari79@gmail.com>
2017-08-01 16:26:20 -0700

    cycle through images

--- MNIST_display_images.py
+++ MNIST_display_images.py
@@ -1,13 +1,19 @@
 import tensorflow as tf
 import cv2
 import numpy as np
+from msvcrt import getch
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-image_check = mnist.test.images[12]
-label_check = mnist.test.labels[12]
-print (label_check)
-image_check=np.array(image_check,dtype='float32')
-image_check=image_check.reshape((28,28))
-cv2.imshow('check',image_check)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+counter=0
+for i in range(1,100):
+    input("Please press a key to continue..")
+        
+    image_check = mnist.test.images[i]
+    label_check = mnist.test.labels[i]
+    print (label_check)
+    image_check=np.array(image_check,dtype='float32')
+    image_check=image_check.reshape((28,28))
+    cv2.imshow('check',image_check)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
