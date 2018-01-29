ea22fffe660a2816aae509c390ac53b8062288db
vivekhari79 <vivekh3@gmail.com>
2018-01-28 23:01:43 -0700

    t

--- Test3.py
+++ Test3.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpt
 import numpy as np
+from PIL import Image
 plt.rcParams['toolbar']='None'
 image=plt.imread("Test.jpg")
 height,width,depth=image.shape
@@ -8,7 +9,8 @@ figsize=350/80,300/80
 fig=plt.figure(figsize=figsize)
 plt.plot([1,2,3])
 plt.subplot(211)
-plt.imshow(image,cmap='gray')
+
+plt.imshow(image,cmap='gray',extent=(1,32,32,64))
 plt.axis('off')
 str_comp_thinks="This is what the computer thinks this is: "
 plt.text(-20,40,str_comp_thinks)
