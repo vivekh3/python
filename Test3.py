b73d5229729d24c48422022283ff50dc39222f34
vivekhari79 <vivekh3@gmail.com>
2018-01-28 22:39:38 -0700

    Test3 matplotlib

--- Test3.py
+++ Test3.py
@@ -1,8 +1,18 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpt
 import numpy as np
+plt.rcParams['toolbar']='None'
 image=plt.imread("Test.jpg")
 height,width,depth=image.shape
-figsize=5,5
+figsize=350/80,300/80
 fig=plt.figure(figsize=figsize)
+plt.plot([1,2,3])
+plt.subplot(211)
+plt.imshow(image,cmap='gray')
+plt.axis('off')
+str_comp_thinks="This is what the computer thinks this is: "
+plt.text(-20,40,str_comp_thinks)
+str_actual="This is what it actually is: "
+plt.text(-20,45,str_actual)
 plt.show()
+
